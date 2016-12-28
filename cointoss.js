b558f2eeb010a196b6c593bb2ae1aad3c9133a69
TacticalBacon00 <GitHub@TacticalBacon00.com>
2016-12-28 11:58:57 -0500

    updated again

--- cointoss.js
+++ cointoss.js
@@ -4,7 +4,25 @@ for (i=0; i<100; i++) {if(Math.floor(Math.random()*2) >= 1) {heads++;} else {tai
   console.log("Heads: " + heads + "; Tails: " + tails);}
 console.log("End result: ");
 console.log("Heads: " + heads + "; Tails: " + tails);
-*/
+
 function toss(){
-	if(Math.floor(Math.random()*2) >= 1) {return "Heads";} else {return "Tails";}
+	if(Math.floor(Math.random()*2) >= 1) {
+		return "Heads";
+		} else {
+			return "Tails";
+			}
 }
+toss();
+*/
+
+var exports = module.exports = {};
+
+exports.results = {
+	toss: function (){
+	if(Math.floor(Math.random()*2) >= 1) {
+		return "Heads";
+		} else {
+			return "Tails";
+			}
+}
+} 
