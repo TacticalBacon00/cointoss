87baa4934d08b94115cefd9c13c2a6fd82048d3a
TacticalBacon00 <GitHub@TacticalBacon00.com>
2016-12-28 11:28:29 -0500

    updated to work

--- cointoss.js
+++ cointoss.js
@@ -5,6 +5,6 @@ for (i=0; i<100; i++) {if(Math.floor(Math.random()*2) >= 1) {heads++;} else {tai
 console.log("End result: ");
 console.log("Heads: " + heads + "; Tails: " + tails);
 */
-function(){
+function toss(){
 	if(Math.floor(Math.random()*2) >= 1) {return "Heads";} else {return "Tails";}
 }
