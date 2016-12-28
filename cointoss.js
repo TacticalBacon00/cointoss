cf9c7ffae0c0218daa94bcbb37db905f4e6fe366
TacticalBacon00 <GitHub@TacticalBacon00.com>
2016-12-28 11:24:57 -0500

    converted js to function

--- cointoss.js
+++ cointoss.js
@@ -1,5 +1,10 @@
+/*
 var heads = 0, tails = 0;
 for (i=0; i<100; i++) {if(Math.floor(Math.random()*2) >= 1) {heads++;} else {tails++;}
   console.log("Heads: " + heads + "; Tails: " + tails);}
 console.log("End result: ");
 console.log("Heads: " + heads + "; Tails: " + tails);
+*/
+function(){
+	if(Math.floor(Math.random()*2) >= 1) {return "Heads";} else {return "Tails";}
+}
